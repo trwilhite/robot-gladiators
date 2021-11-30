247bce86ad171744100609cec60c76189ba7c4bc
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2021-11-30 00:22:53 -0500

    debugged to fix game end after first enemy dies

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,9 +19,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+  // repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0){
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -71,9 +70,12 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
